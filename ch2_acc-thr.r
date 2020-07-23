3ed110bfc3da407aaba5952ad38046d785eb6d59
djwang <dajun.wang@nie.edu.sg>
2020-07-23 08:45:11 +0800

    saved

--- ch2_acc-thr.r
+++ ch2_acc-thr.r
@@ -31,10 +31,6 @@ acc.n.var = acc.n %>%
 
 acc.n.var = cbind(acc.n.var, acc.n.var.col.names)
 
-acc.n.var$threshold.z = ifelse(acc.n.var$var.z > 1000, "high", "low")
-acc.n.var$threshold.y = ifelse(acc.n.var$var.y > 1000, "high", "low")
-acc.n.var$threshold.x = ifelse(acc.n.var$var.x > 1000, "high", "low")
-
 gps = gps %>%
   select(sg.time, location_long, location_lat) %>%
   rename(longitude = location_long,
@@ -47,7 +43,7 @@ for (i in 1:nrow(gps)) {
 gps$time.diff[29] = 60
 acc.n.var$burst = NULL
 
-match = read.csv('../2020-07-21_acc-thr_gps-cleaned.csv') # hand-cleaned gps data
+match = read.csv('2020-07-21_acc-thr_gps-cleaned.csv') # hand-cleaned gps data
 match$burst = match$X
 
 ggplot(match, aes(x = burst)) +
@@ -58,6 +54,76 @@ ggplot(match, aes(x = burst)) +
 
 
 
+```{r identifing the sequential threshold value}
+move.seq = df.all %>%
+  filter(behaviour %in% c("forage", "walk", "run"))
+
+still.seq = df.all %>%
+  filter(behaviour %in% c("lie", "sit", "stand"))
+
+
+runs = rle(still.seq$var.z > 10000) 
+# rle() identifies ($values) and computes the number of time ($length) that value exceeds 10,000
+# in the "still" behaviour dataset
+# This arbitary value plucked from the table above. Axis Z is likely representing the surge axis. This is to prevent the postural changes of the dogs from activating the acc-thr of the collar in non-movement based behaviours such as barking, or lying.
+
+myruns = which(runs$values == TRUE & runs$lengths > 5) 
+# a dataframe that indicates the arguments set above; $value >10000 and $length > 2 (meaning happens more than twice in a row). 
+
+any(myruns)
+
+runs.lengths.cumsum = cumsum(runs$lengths)
+ends = runs.lengths.cumsum[myruns] 
+# To find the 'end' of each sequence as defined by myruns's arguments
+# which indices more than 10,000 and the duration of that sequence.
+
+newindex = ifelse(myruns>1, myruns-1,0)
+starts = runs.lengths.cumsum[newindex] + 1
+
+diff = starts - ends
+
+
+move.runs = rle(move.seq$var.z > 10000) 
+
+move.myruns = which(move.runs$values == TRUE & move.runs$lengths > 2) 
+# a dataframe that indicates the arguments set above; $value >10000 and $length > 2 (meaning happens more than twice in a row). 
+
+any(move.myruns)
+
+move.runs.lengths.cumsum = cumsum(move.runs$lengths)
+move.ends = move.runs.lengths.cumsum[move.myruns] 
+# To find the 'end' of each sequence as defined by myruns's arguments
+# which indices more than 10,000 and the duration of that sequence.
+
+move.newindex = ifelse(move.myruns>1, move.myruns-1,0)
+move.starts = move.runs.lengths.cumsum[move.newindex] + 1
+
+diff = starts - ends
+
+
+
+
+
+
+still.thr = list()
+still.thr.runs = list()
+still.length.cumsum = list()
+still.ends = list()
+still.starts = list()
+still.index = list()
+for (i in 1:5) {
+  still.thr[[i]] = rle(still.seq$var.z > i*5000)
+  still.thr.runs[[i]] = which(still.thr[[i]]$values == TRUE & still.thr[[i]]$lengths >= 5)
+  still.length.cumsum[[i]] = cumsum(still.thr[[i]]$length)
+  still.index[[i]] = ifelse(still.thr.runs[[i]]>1, still.thr.runs[[i]]-1,0) + 1
+  still.ends[[i]] = cumsum(still.thr[[i]]$length)[still.thr.runs[[i]]]
+  still.starts[[i]] = still.length.cumsum[[i]][still.index[[i]]] + 1
+}
+
+
+print(still.seq$var.z[starts[1]:ends[1]])
+
+```
 
 
 
